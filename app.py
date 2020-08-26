fe6508a167d8553f4713d38b941750141bdb1cc5
Harshitha <10524115@mydbs.ie>
2020-08-27 02:23:26 +0530

    make it better

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
-chrome_options = webdriver.chromeOptions()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-ussage")
